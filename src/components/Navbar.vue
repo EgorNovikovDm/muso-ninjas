2e5c754df9179f7cde2f81a3fc20c96c0a90c3cd
Egor Novikov <egor.novikov.1979@yandex.ru>
2021-05-29 20:09:29 +0300

    first commit

---
+++ src/components/Navbar.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="navbar">
+    <nav>
+      <h1>
+        <router-link :to="{ name: 'Home'}">Muso Ninjas</router-link>
+      </h1>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+</style>
